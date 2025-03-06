fc76b9ac11f5bd23b1200970102d9d9d6d87f1ef
Sebastian <ruben.sebastian.aleman@gmail.com>
2025-03-06 18:30:34 -0500

    added logout button for testers oops

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -39,6 +39,8 @@ export default async function Account() {
         <main>
           <h1>Welcome, {applicant!.full_name}!</h1>
           <p>Thanks for helping with testing!</p>
+          <br />
+          <LogoutButton />
         </main>
       )
     }
