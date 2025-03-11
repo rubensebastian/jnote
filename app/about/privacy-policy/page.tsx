a19423ab37a92613a2b1c7ec6a32a18c0fd72021
Sebastian <ruben.sebastian.aleman@gmail.com>
2025-03-11 17:14:55 -0400

    privacy policy for chrome extension

---
+++ app/about/privacy-policy/page.tsx
@@ -0,0 +1,10 @@
+import TermsAndConditions from '@/components/TermsAndConditions'
+
+export default function PrivacyPolicy() {
+  return (
+    <main>
+      <h1>Privacy Policy</h1>
+      <TermsAndConditions />
+    </main>
+  )
+}
