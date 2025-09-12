75770e0161bcd6db6d8ab0515a0fea88ca65ab1e
Sebastian <ruben.sebastian.aleman@gmail.com>
2025-09-12 17:36:23 -0400

    resume word

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -6,7 +6,7 @@ export default function Menu() {
       <ol className='flex flex-row items-center justify-end h-12 bg-gray-300 px-4'>
         <MenuLink href='/upgrade' description='Upgrade to Pro' />
         <MenuLink href='/jobs' description='Jobs' />
-        <MenuLink href='/resume' description='Resumé' />
+        <MenuLink href='/resume' description='Résumé' />
         <MenuLink href='/about' description='About JobNote' />
         <MenuLink href='/account' description='Your Account' />
       </ol>
