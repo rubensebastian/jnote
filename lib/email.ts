8b90e7734243ff8f1f1db276a79d1ef807012298
Sebastian <ruben.sebastian.aleman@gmail.com>
2025-12-03 17:49:12 -0500

    remove no-reply from email

--- lib/email.ts
+++ lib/email.ts
@@ -6,7 +6,7 @@ export async function sendVerificationEmail(email: string, token: string) {
   const verificationLink = `${process.env.NEXT_PUBLIC_BASE_URL}/verify?token=${token}`
 
   await resend.emails.send({
-    from: 'JobNote <no-reply@jobnote.app>',
+    from: 'JobNote <admin@jobnote.app>',
     to: email,
     subject: 'Please Verify Your Email Address',
     html: `<p>Please verify your email by clicking the link below:</p>
